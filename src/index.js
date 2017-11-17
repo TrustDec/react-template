228dd77894631927f609aeb454d4126c425c3cf5
TrustTheBoy <603049583@qq.com>
2017-11-17 15:46:47 +0800

    删除重复引入

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'components/App/App';
 
 renderWithHotReload(App);
